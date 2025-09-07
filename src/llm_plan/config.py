2d152c98ef39a7157c5e0e59e98eb23348825621
Ping Zhu <pingchuan.zhu@st-hildas.ox.ac.uk>
2025-09-07 02:42:03 +0100

    Removed Path surrounding solver_binary, universal_validator and universal_validator_bin. For compatibility on different OS

--- src/llm_plan/config.py
+++ src/llm_plan/config.py
@@ -3,6 +3,9 @@ import json
 
 # Assume you always run Python from project_root
 _config_file = Path.cwd() / "config.json"
+_secondary_config_file = Path(__file__).parent.parent.parent / "config.json"
+if not _config_file.exists() and _secondary_config_file.exists():
+    _config_file = _secondary_config_file
 
 if _config_file.exists():
     with open(_config_file) as f:
@@ -22,6 +25,7 @@ ENVIRONMENTS_JSON_SAMPLE_PATH = Path(CONFIG.get("environments_json_sample_path",
 ENVIRONMENTS_PYTHON_PATH_SAMPLE = Path(
     CONFIG.get("environments_python_path_sample", "")
 )
-SOLVER_BINARY = Path(CONFIG.get("solver_binary", ""))
+SOLVER_BINARY = CONFIG.get("solver_binary", "")
 SOLVER_ARGS = CONFIG.get("solver_args", [])
+UNIVERSAL_VALIDATOR = CONFIG.get("universal_validator", "")
 UNIVERSAL_VALIDATOR_BIN = CONFIG.get("universal_validator_bin", "")
